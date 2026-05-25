e48e80a6a474ac138e26277ad82ba94c7acbcc7b
Ahmad Abdullahi Bayero <125829469+elakhmad2020@users.noreply.github.com>
2026-05-25 08:59:43 +0000

    fix: remove AdminApp from index.js, single entry point

--- spanhc/src/index.js
+++ spanhc/src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import AdminApp from './AdminApp';
 import reportWebVitals from './reportWebVitals';
-
-const isAdmin = window.location.pathname.startsWith('/admin');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {isAdmin ? <AdminApp /> : <App />}
+    <App />
   </React.StrictMode>
 );
 
